1f12e995eb864e8363a9ac3f0293a02151db1b02
AdarshGrewal <adarshgrewal@gmail.com>
2024-10-28 12:16:57 +0530

    zircon: Switch to common xiaomi sensor aidl
    Change-Id: Ib25abd840613c328723908047225fdb2f94c377f

--- device.mk
+++ device.mk
@@ -372,7 +372,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors-service.xiaomi-multihal
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
