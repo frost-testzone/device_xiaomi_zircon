b103f096f092445f5cc4106946befc1a8e136ab2
AdarshGrewal <adarshgrewal@gmail.com>
2024-11-10 16:36:48 +0530

    zircon: Switch to common mediatek health aidl
    Change-Id: I09dde0e7ad897a6f1d44c43ab7fc02a40da88b21

--- device.mk
+++ device.mk
@@ -204,8 +204,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.example \
-    android.hardware.health-service.example_recovery
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
 
 PRODUCT_PACKAGES += \
     android.hardware.health@1.0.vendor:64
