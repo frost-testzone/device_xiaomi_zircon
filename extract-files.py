87e7339a72d6984e1a8429a0206b0fa7d9105176
Giovanni Ricca <giovanniricca@proton.me>
2025-01-08 12:55:07 +0100

    zircon: `libshim_sink.so` -> `libaudioclient_shim.so`
    Change-Id: I27c7cea3f2b7feda9736b9f1b1aa52db9202736e
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     'system_ext/lib64/libsink-mtk.so': blob_fixup()
-        .add_needed('libshim_sink.so'),
+        .add_needed('libaudioclient_shim.so'),
     'system_ext/lib64/libsource.so': blob_fixup()
         .add_needed('libui_shim.so'),
     'odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so': blob_fixup()
