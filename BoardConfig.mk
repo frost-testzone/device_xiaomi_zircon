3af2862861efce056c8754d636712b39821d7088
Giovanni Ricca <giovanniricca@proton.me>
2025-02-19 17:10:29 +0100

    zircon: Inherit common Mediatek FCM
    Change-Id: I2807b7f2b0c86305fea56f28f0137ea2166ebade
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,6 +46,7 @@ TARGET_SCREEN_DENSITY := 480
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
