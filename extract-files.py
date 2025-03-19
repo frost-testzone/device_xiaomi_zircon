0f137fead20db9788b7f075dbd1f7fba7545264d
Nauval Rizky <enuma.alrizky@gmail.com>
2025-03-19 10:57:56 +0100

    zircon: Restrict `mi_thermald` from CPU hotplugging
    * hotplug data will be parsed but not applied to the node.
    
    Change-Id: If5664212142f5bb3a1e8c937b7054f51a1055361
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -58,6 +58,8 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.mitee': blob_fixup()
         .replace_needed('android.hardware.security.keymint-V2-ndk.so', 'android.hardware.security.keymint-V3-ndk.so')
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
+    'vendor/bin/mi_thermald': blob_fixup()
+        .binary_regex_replace(b'%d/on', b'%d/..'),
     'vendor/etc/init/vendor.xiaomi.hardware.vibratorfeature.service.rc': blob_fixup()
         .regex_replace('/odm/bin', '/vendor/bin'),
     ('vendor/lib64/hw/hwcomposer.mtk_common.so', 'vendor/lib64/libmialgoengine.so', 'vendor/lib64/mt6886/libcam.hal3a.ctrl.so', 'vendor/lib64/mt6886/libcam.hal3a.so', 'vendor/lib64/mt6886/libmtkcam_cputrack.so', 'vendor/lib64/mt6886/libmtkcam_request_requlator.so'): blob_fixup()
