ecb6e46867f32789e7cb5df373ef6e245980fec1
Arian <arian.kulmer@web.de>
2025-05-22 11:35:52 +0200

    zircon: udfps: Disable lhbm when authentication is finished
    We previously missed the case when the finger is rejected the
    fifth time and neither cancel nor a successful acquired message
    was sent, leading to lhbm staying on forever. These new methods
    should cover all cases in which the authentication is over. We
    do not need to handle onAcquired and cancel explicitly anymore,
    so drop the corresponding methods.
    
    Change-Id: Ibc284bc250e60a46d220f6a8f4c82908cd1f0776
    Signed-off-by: Frost <frost.github@proton.me>

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2024 The LineageOS Project
+ * Copyright (C) 2024-2025 The LineageOS Project
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #define LOG_TAG "UdfpsHandler.zircon"
 
-#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 
@@ -28,8 +27,6 @@
 #define PARAM_FOD_RELEASED 0
 
 #define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
-
-using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
 
@@ -155,14 +152,9 @@ class ZirconUdfpsHandler : public UdfpsHandler {
         ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
     }
 
-    void onAcquired(int32_t result, int32_t vendorCode) {
-        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
-        if (static_cast<AcquiredInfo>(result) == AcquiredInfo::GOOD) {
-            onFingerUp();
-        }
-    }
+    void onAuthenticationSucceeded() { onFingerUp(); }
 
-    void cancel() { LOG(DEBUG) << __func__; }
+    void onAuthenticationFailed() { onFingerUp(); }
 
   private:
     fingerprint_device_t* mDevice;
