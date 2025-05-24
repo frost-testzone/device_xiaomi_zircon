48bea888c766b561f2ec780cc8f7868c7c46ceda
Arian <arian.kulmer@web.de>
2025-05-24 13:15:15 +0200

    zircon: udfps: Disable lhbm on most acquired events
    Change-Id: I0d2e340a553a4c9e41666f6f422805cecb15a68d
    Signed-off-by: Frost <frost.github@proton.me>

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -6,6 +6,7 @@
 
 #define LOG_TAG "UdfpsHandler.zircon"
 
+#include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 #include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 
@@ -27,6 +28,8 @@
 #define PARAM_FOD_RELEASED 0
 
 #define DISP_FEATURE_PATH "/dev/mi_display/disp_feature"
+
+using ::aidl::android::hardware::biometrics::fingerprint::AcquiredInfo;
 
 namespace {
 
@@ -150,6 +153,26 @@ class ZirconUdfpsHandler : public UdfpsHandler {
                 .local_hbm_value = LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP,
         };
         ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &displayLhbmRequest);
+    }
+
+    void onAcquired(int32_t result, int32_t vendorCode) {
+        LOG(DEBUG) << __func__ << " result: " << result << " vendorCode: " << vendorCode;
+        switch (static_cast<AcquiredInfo>(result)) {
+            case AcquiredInfo::GOOD:
+            case AcquiredInfo::PARTIAL:
+            case AcquiredInfo::INSUFFICIENT:
+            case AcquiredInfo::SENSOR_DIRTY:
+            case AcquiredInfo::TOO_SLOW:
+            case AcquiredInfo::TOO_FAST:
+            case AcquiredInfo::TOO_DARK:
+            case AcquiredInfo::TOO_BRIGHT:
+            case AcquiredInfo::IMMOBILE:
+            case AcquiredInfo::LIFT_TOO_SOON:
+                onFingerUp();
+                break;
+            default:
+                break;
+        }
     }
 
     void onAuthenticationSucceeded() { onFingerUp(); }
