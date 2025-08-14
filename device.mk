d2ae4aaf92c3fbafc1fe87146b4dbb786213f957
Mashopy <eliasgheeraert@gmail.com>
2025-08-14 21:38:47 +0200

    zircon: Switch to AIDL bluetooth service
    Change-Id: I40550f0f2033e35ea609401f38f7646cb79abe14
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -93,6 +93,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
+    android.hardware.bluetooth-service.mediatek \
     android.hardware.bluetooth.audio-impl:64
 
 PRODUCT_COPY_FILES += \
