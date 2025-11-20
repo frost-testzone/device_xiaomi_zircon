bbaba6b166f418b4bbb7e2bcedb8923230767672
rio004 <riyon336@gmail.com>
2025-11-20 23:44:42 +0000

    zircon: Switch to prebuilt audio impl
    Change-Id: I8fdcd476042bf84328748ccaded65a70335727f8
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -41,7 +41,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio@7.1-impl \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.audio.service
 
