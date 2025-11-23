a51e0b12d650a29228b8ee263ff9c44d00a8772e
Frost <frost.github@proton.me>
2025-11-23 17:52:34 +0530

    zircon: Add missing cleaned up `init_conninfra.rc`
    Change-Id: I11658e0e8473d4dddf5c6b8fd66630acb81d3c86
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.enableswap \
     fstab.mt6886 \
+    init_conninfra.rc \
     init.connectivity.common.rc \
     init.fingerprint.rc \
     init.mt6886.rc \
