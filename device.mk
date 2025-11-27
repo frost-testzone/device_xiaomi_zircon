2ca0fc96ff22a92f446599e156394d1cf89cfcca
Frost <frost.github@proton.me>
2025-11-27 15:18:10 +0530

    zircon: Remove Aperture overlay
    * We override Aperture with MiuiCamera.
    
    Change-Id: I962878c82b5e47aa662f18ec39fa199ad2241b05
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -242,7 +242,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    ApertureOverlayZircon \
     VoltagePowerOffAlarmOverlayZircon \
     VoltageSettingsOverlayZircon
 
