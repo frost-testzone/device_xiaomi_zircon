f3bad6d6ba284324ea01d708ae850c2f226e8edb
Frost <frost.github@proton.me>
2025-12-01 18:16:15 +0530

    Revert "zircon: udfps: Move HBM requests back to FingerDown"
    This reverts commit 0549a27482c98b76e636a84cb8c720aa4b01843d.
    
    Change-Id: I89848e43b0f3a14f639ea32dd7dd2c16ed8f5898
    Signed-off-by: Frost <frost.github@proton.me>

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -119,6 +119,14 @@ class ZirconUdfpsHandler : public UdfpsHandler {
                 bool pressed = readBool(fd);
                 mDevice->extCmd(mDevice, COMMAND_FOD_PRESS_STATUS,
                                 pressed ? PARAM_FOD_PRESSED : PARAM_FOD_RELEASED);
+
+                // Request HBM
+                disp_local_hbm_req req;
+                req.base.flag = 0;
+                req.base.disp_id = MI_DISP_PRIMARY;
+                req.local_hbm_value = pressed ? LHBM_TARGET_BRIGHTNESS_WHITE_1000NIT
+                                              : LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP;
+                ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
             }
         }).detach();
 
@@ -210,11 +218,6 @@ class ZirconUdfpsHandler : public UdfpsHandler {
     }
 
     void setFingerDown(bool pressed) {
-        disp_local_hbm_req req;
-        req.base.flag = 0;
-        req.base.disp_id = MI_DISP_PRIMARY;
-        req.local_hbm_value = pressed ? LHBM_TARGET_BRIGHTNESS_WHITE_1000NIT : LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP;
-        ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
         int buf[MAX_BUF_SIZE] = {MI_DISP_PRIMARY, Touch_Fod_Enable, pressed ? 1 : 0};
         ioctl(touch_fd_.get(), TOUCH_IOC_SET_CUR_VALUE, &buf);
     }
