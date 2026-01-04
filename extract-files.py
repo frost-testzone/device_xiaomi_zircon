8d2933a0bd510248a42ac0d1bb31e06077d5aad0
Mashopy <eliasgheeraert@gmail.com>
2026-01-04 13:27:12 +0100

    zircon: Properly patch blobs related to Stagefright
    Audio primary HAL doesn't have anything related to Stagefright, so there is no need to add
    libstagefright_foundation-v33.so dependency into it. Same goes for the C2 service and dolby
    services as it's the C2 and dolby vdec/venc and plugin libs that has references for it.
    
    And let's patch the sensor HAL too just in case.
    
    There is no need to patch the system libs that depends over it as they aren't built
    over Android 13 like the vendor libs are.
    
    Change-Id: I9096179f8f89ef2c23f8e22e04228f9f39bbf60a
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -49,8 +49,6 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('start', 'enable'),
     'vendor/bin/hw/android.hardware.graphics.composer@3.1-service': blob_fixup()
         .replace_needed('android.hardware.graphics.composer@2.1-resources.so', 'android.hardware.graphics.composer@2.1-resources-v34.so'),
-    ('vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b', 'vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service', 'vendor/bin/hw/vendor.dolby.media.c2@1.0-service'): blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
     'vendor/bin/hw/android.hardware.security.keymint@2.0-service.mitee': blob_fixup()
         .add_needed('android.hardware.security.rkp-V3-ndk.so')
         .replace_needed('android.hardware.security.keymint-V2-ndk.so', 'android.hardware.security.keymint-V3-ndk.so'),
@@ -65,7 +63,6 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/vendor.mediatek.hardware.pq_aidl-V1-ndk.so', 'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V2-ndk.so'): blob_fixup()
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so')
         .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
         .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00')
         .replace_needed('libalsautils.so', 'libalsautils-v33.so')
@@ -73,6 +70,13 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/hw/hwcomposer.mtk_common.so', 'vendor/lib64/libmialgoengine.so', 'vendor/lib64/mt6886/libcam.hal3a.ctrl.so',
     'vendor/lib64/mt6886/libcam.hal3a.so', 'vendor/lib64/mt6886/libmtkcam_cputrack.so', 'vendor/lib64/mt6886/libmtkcam_request_requlator.so'): blob_fixup()
         .add_needed('libprocessgroup_shim.so'),
+    ('vendor/lib64/hw/sensors.mediatek.V2.0.so', 'vendor/lib64/soundfx/libswdap.so', 'vendor/lib64/soundfx/libdlbvol.so',
+    'vendor/lib64/libcodec2_mtk_c2store.so', 'vendor/lib64/libcodec2_mtk_vdec.so', 'vendor/lib64/libcodec2_mtk_venc.so',
+    'vendor/lib64/libcodec2_soft_common_prebuilt.so', 'vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so', 'vendor/lib64/libcodec2_vpp_AISR_plugin.so',
+    'vendor/lib64/libcodec2_vpp_dolby_plugin.so', 'vendor/lib64/libcodec2_vpp_gc_plugin.so', 'vendor/lib64/libcodec2_vpp_mi_plugin.so',
+    'vendor/lib64/libcodec2_vpp_qt_plugin.so', 'vendor/lib64/libcodec2_vpp_rs_plugin.so', 'vendor/lib64/libdlbdsservice.so',
+    'vendor/lib64/libdlbpreg.so'): blob_fixup()
+        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     ('vendor/lib64/hw/mt6886/vendor.mediatek.hardware.pq_aidl-impl.so', 'vendor/lib64/libaudiocloudctrl.so', 'vendor/lib64/libpqxmlparser.so',
     'vendor/lib64/librt_extamp_intf.so', 'vendor/lib64/mt6886/libmmlpqImpl.so'): blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
@@ -88,9 +92,6 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/lib64/libcodec2_vpp_AIMEMC_plugin.so', 'vendor/lib64/libcodec2_vpp_AISR_plugin.so'): blob_fixup()
         .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so')
         .replace_needed('android.hardware.graphics.common-V3-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
-    ('vendor/lib64/soundfx/libswdap.so', 'vendor/lib64/soundfx/libdlbvol.so', 'vendor/lib64/libcodec2_soft_common_prebuilt.so',
-    'vendor/lib64/libdlbdsservice.so', 'vendor/lib64/libdlbpreg.so'): blob_fixup()
-        .replace_needed('libstagefright_foundation.so', 'libstagefright_foundation-v33.so'),
     'vendor/lib64/mt6886/libneuralnetworks_sl_driver_mtk_prebuilt.so': blob_fixup()
         .clear_symbol_version('AHardwareBuffer_allocate')
         .clear_symbol_version('AHardwareBuffer_createFromHandle')
