ff51f4a07f6ca6f7c21affc65e82bdb51ad72fe1
Mashopy <eliasgheeraert@gmail.com>
2026-06-04 20:12:22 +0200

    zircon: Properly enable LazyRCU
    Change-Id: I2c8ecb24b3e5a0a8f173b0b0f7f40006fecec87b
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,7 +67,7 @@ BOARD_KERNEL_CMDLINE := \
     cgroup_disable=memory \
     log_buf_len=1024K \
     rcu_nocbs=all \
-    rcutree.enable_rcu_lazy=1 \
+    rcutree.enable_rcu_lazy \
     sysctl.kernel.sched_pelt_multiplier=4
 
 TARGET_KERNEL_CONFIG := \
