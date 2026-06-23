182dbbfbb81c2121ab2471bfaac92c7f7ae7516a
Frost <frost.github@proton.me>
2026-06-23 13:15:36 +0530

    Revert "zircon: Update VT from rothko A16"
    This reverts commit fc9c085cc132d0374cc98889ef2f58610ad53365.
    
    Change-Id: I20d5c475a8d537f415c58df41c70b536d5b381de
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -50,6 +50,10 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('start', 'enable'),
     'system_ext/lib64/libimsma.so': blob_fixup()
         .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsink-mtk.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
     'system_ext/priv-app/ImsService/ImsService.apk': blob_fixup()
         .apktool_patch('blob-patches/ImsService'),
     'vendor/bin/hw/android.hardware.graphics.composer@3.1-service': blob_fixup()
