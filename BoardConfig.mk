d281808e28ff6126b3659a0467c128fc46ef8659
Frost <frost.github@proton.me>
2026-06-24 14:12:38 +0530

    zircon: Use clang-r563880c for now
    Change-Id: Icd67ae51e3e63777e769613b89177525cf9859b5
    Signed-off-by: Frost <frost.github@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,6 +71,9 @@ BOARD_KERNEL_CMDLINE := \
     rcutree.enable_rcu_lazy \
     sysctl.kernel.sched_pelt_multiplier=4
 
+TARGET_KERNEL_CLANG_VERSION := r563880c
+TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
+
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/mgk_64_k515.config \
